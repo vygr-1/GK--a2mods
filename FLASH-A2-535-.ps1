feef575a4feb31bf27b5df71f0a3e5a886d41cb1
vygr-1 <zz.gdrive19@gmail.com>
2025-03-17 07:15:27 +0700

    u

--- FLASH-A2-535-.ps1
+++ FLASH-A2-535-.ps1
@@ -5,7 +5,7 @@
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-###   ###   ###   ###   ###   ###   ###   ###   ###   ###
+###   ###   ###   ###   ###   ###   ###
 
 
 
